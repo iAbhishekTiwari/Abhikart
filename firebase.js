2a5d9ebeae02020abb08fc6842fc54813df04f01
I_AbhishekTiwari <107391841+iAbhishekTiwari@users.noreply.github.com>
2022-07-23 13:12:02 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyChMqwakXO1f79cZ9jWRNJUUcBg0_ASnns",
+    authDomain: "abhikart.firebaseapp.com",
+    projectId: "abhikart",
+    storageBucket: "abhikart.appspot.com",
+    messagingSenderId: "149093867571",
+    appId: "1:149093867571:web:eddceb2d1f3f816b454346",
+    measurementId: "G-Q83QHV823K"
+  };
